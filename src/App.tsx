5263b6fe6a47cc3a8d6171013ea7b4a03b8b88d5
satake yusuke <satake@osa.connected.co.jp>
2020-09-07 14:36:56 +0900

    dnd imprement

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FC } from 'react';
 import './App.css';
+import { useDropzone } from 'react-dropzone';
 
-function App() {
+const App: FC = () => {
+
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+  console.log(acceptedFiles, 'file dnd check')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div {...getRootProps({ className: 'dropzone' })}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </div>
     </div>
   );
 }
